33d787160f006c490984c2a00c86afe5ea2e2138
Robert <robertyeam@gmail.com>
2021-04-10 18:21:31 -0700

    fixed a bug

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # code-quiz
+Robert Yeam
+Code Quiz
+
 Week 4 homework
+In this project I made a quiz that goes through a series of questions about JavaScript, html, and css and records the score
+
+Live at:
 
 https://roberty157.github.io/code-quiz/
--- assets/js/script2.js
+++ assets/js/script2.js
@@ -26,6 +26,7 @@ var endBlock = $("#endBlock");
 
 
 function init(){
+    secondsLeftEl.text("20");
     highScoreLinkEl.css("display","block");
 
     introBlock.css("display","block");
@@ -60,6 +61,24 @@ function init(){
 function getRandomInt(max){
     return Math.floor(Math.random()* max);
 }
+function randomOrder(length){
+    //returns array of numbers from 0 to length-1 in a randomized order
+
+    //if randomOrder(3)
+    //return array of 0,1,2 in random order
+    var lst =[];
+    for(var i=0;i<length;i++){
+        lst.push(i);
+    }
+    //lst = [0,1,2]
+    var newLst = [];
+    for(var j =0;j<length;j++){
+        var index = getRandomInt(lst.length);
+        newLst.push(lst[index]);
+        lst.splice(index,1);
+    }
+    return newLst;
+}
 
 //objects of possible questions and their possible answers
 var q0 = {
@@ -67,10 +86,10 @@ var q0 = {
     //correct: "alerts",
     //incorrect: ["strings","booleans","numbers"],
     responseLst:[
-        {"response":"alerts",correct:true},
-        {"response":"strings",correct:false},
-        {"response":"booleans",correct:false},
-        {"response":"numbers",correct:false}
+        {response:"alerts",correct:true},
+        {response:"strings",correct:false},
+        {response:"booleans",correct:false},
+        {response:"numbers",correct:false}
     ]
 }
 var q1 ={
@@ -78,10 +97,10 @@ var q1 ={
     //correct: "curly brackes",
     //incorrect: ["square brackets", "parentheses","quotation marks"],
     responseLst:[
-        {"response":"curly braces", correct:true},
-        {"response":"square brackets",correct:false},
-        {"response":"parentheses", correct:false},
-        {"response":"quotation marks", correct:false}
+        {response:"curly braces", correct:true},
+        {response:"square brackets",correct:false},
+        {response:"parentheses", correct:false},
+        {response:"quotation marks", correct:false}
     ]
 }
 var q2 ={
@@ -89,21 +108,57 @@ var q2 ={
     //correct: "quotation marks",
     //incorrect: ["square brackets", "curly braces", "backticks"],
     responseLst:[
-        {"response":"quotation marks",correct:true},
-        {"response":"square brackets", correct:false},
-        {"response":"curly braces", correct:false},
-        {"response":"backticks", correct:false}
+        {response:"quotation marks",correct:true},
+        {response:"square brackets", correct:false},
+        {response:"curly braces", correct:false},
+        {response:"backticks", correct:false}
     ]
 }
-
+var q3 ={
+    question:"Bootstrap uses a ____ column system",
+    responseLst:[
+        {response:"12",correct:true},
+        {response:"10",correct:false},
+        {response:"4",correct:false},
+        {response:"16",correct:false}
+    ]
+}
+var q4 ={
+    question:"Using jQuery, create an h1 tag",
+    responseLst:[
+        {response:"$('<h1>')",correct:true},
+        {response:"document.createElement('h1')",correct:false},
+        {response:"<h1></h1>",correct:false},
+        {response:"document.append('h1')",correct:false}
+    ]
+}
+var q5 ={
+    question:"Array values must be enclosed within ____",
+    responseLst:[
+        {response:"square brackets",correct:true},
+        {response:"parentheses",correct:false},
+        {response:"curly braces",correct:false},
+        {response:"quotation marks",correct:false}
+    ]
+}
+var q6 ={
+    question:"find the length of an array Lst",
+    responseLst:[
+        {response:"Lst.length",correct:true},
+        {response:"len(Lst)",correct:false},
+        {response:"lst.length()",correct:false},
+        {response:"lenof Lst",correct:false}
+    ]
+}
 //define function to init quiz
 
 //var score = 0;
 function initQuiz(){
     questionBlock.css("display","block");
+
     var score = 0;
 
-    var timeLeft = 20;
+    var timeLeft = 30;
     var timeInterval = setInterval(function(){
         
         
@@ -118,13 +173,50 @@ function initQuiz(){
         }
         
     },1000);
-
+    function genResponses(responseLst){
+        var indexLst = randomOrder(responseLst.length);
+        for(var i=0;i<responseLst.length;i++){
+            var index = indexLst[i];
+            var responseEl = $("<li>");
+            var responseObj = responseLst[index];
+            var responseTxt = responseObj.response;
+            responseEl.text(responseTxt);
+            responsesEl.append(responseEl);
+            responseEl.on("click",function(){
+                //console.log(responseEl.textContent);
+                if(questionList.length > 0){
+                    //resultEl = $("<div>");
+                    //resultEl.attr("id","result");
+                    if(responseObj.correct === true){
+                        resultEl.text("Correct");
+                        //add one to the score
+                        score++;
+                    }else{
+                        resultEl.text("Wrong");
+                        //subtract time if wrong
+                        timeLeft -= 3;
+                    }
+                    //console.log(responseObj);
+                    //questionBlockEl.append(resultEl);
+                    setTimeout(function(){
+                        resultEl.text("");
+                        //console.log(questionEl.children());
+                        //questionBlockEl.children(resultEl).remove();
+                    },1000);
+                }
+                genQuestion();
+            });
+        }
+    }
 
     //load questions
-    var questionList = [q0,q1,q2];
+    var questionList = [q0,q1,q2,q3,q4,q5,q6];
+    //console.log(questionList);
+    //console.log("questionLst",questionList);
     function genQuestion(){
-        responsesEl.text("");
-        questionEl.text("");
+        responsesEl.empty();
+        
+        questionEl.empty();
         if(questionList.length ===0){
             //console.log("quiz finished");
             //questionEl.textContent ="Quiz finished";
@@ -136,58 +228,72 @@ function initQuiz(){
         else{
             var randomIntQ = getRandomInt(questionList.length);
             var qObj = questionList[randomIntQ];
+
+            //console.log("qObj",qObj);
             questionList.splice(randomIntQ,1);
+
             questionEl.text(qObj["question"]);
-            var responses = qObj.responseLst;
+            //console.log("qObj.responseLst",qObj.responseLst);
+            var responseLst = qObj.responseLst;
+            //console.log("responses list",responses);
         
-            
-            
+            genResponses(responseLst);
             //console.log(responses);
             //console.log(responses);
+            /*
             for(var i=0;i<4;i++){
-            const responseEl = $("<li>");
-            
-
-            var index = getRandomInt(responses.length);
-            responseEl.text(responses[index].response);
-
-            const responseObj = responses[index];
-            responseEl.on("click",function(){
-                //console.log(responseEl.textContent);
-
-                if(questionList.length > 0){
-                    //resultEl = $("<div>");
-                    //resultEl.attr("id","result");
-                    if(responseObj.correct === true){
-                        resultEl.text("Correct");
-                        //add one to the score
-                        score++;
-                    }else{
-                        resultEl.text("Wrong");
-
-                        //subtract time if wrong
-                        timeLeft -= 3;
-                    }
-                    //console.log(responseObj);
-                    //questionBlockEl.append(resultEl);
-                    setTimeout(function(){
-                        resultEl.text("");
-
-                        //console.log(questionEl.children());
-                        //questionBlockEl.children(resultEl).remove();
-                    },1000);
-                }
+                const responseEl = $("<li>");
                 
-                genQuestion();
-            });
 
-            responses.splice(index,1);
-            responsesEl.append(responseEl);
+                var index = getRandomInt(responses.length);
+                var responseTxt = responses[index];
+                //console.log(responses[0]);
+                responseTxt= responseTxt.response;
+                //console.log(responseTxt);
+                responseEl.text(responseTxt);
+                //console.log("response",responses[index].response);
+
+                const responseObj = responses[index];
+                responseEl.on("click",function(){
+                    //console.log(responseEl.textContent);
+
+                    if(questionList.length > 0){
+                        //resultEl = $("<div>");
+                        //resultEl.attr("id","result");
+                        if(responseObj.correct === true){
+                            resultEl.text("Correct");
+                            //add one to the score
+                            score++;
+                        }else{
+                            resultEl.text("Wrong");
+
+                            //subtract time if wrong
+                            timeLeft -= 3;
+                        }
+                        //console.log(responseObj);
+                        //questionBlockEl.append(resultEl);
+                        setTimeout(function(){
+                            resultEl.text("");
+
+                            //console.log(questionEl.children());
+                            //questionBlockEl.children(resultEl).remove();
+                        },1000);
+                    }
+                    
+                    genQuestion();
+                });
+
+                responses.splice(index,1);
+                responsesEl.append(responseEl);
             }
+            */
+            //console.log(responses);
         }
     }
     genQuestion();
 }
+
+
 
 
 //high score page
@@ -206,7 +312,8 @@ function saveScore(initials,score){
     }
 }
 function highScorePage(){
-    highScoreLinkEl.css("display","none");
+    //highScoreLinkEl.css("display","none");
+    scoreListEl.empty();
 
     introBlock.css("display","none");
     endBlock.css("display","none");
@@ -225,7 +332,7 @@ function highScorePage(){
     highScores.sort(function(a,b){return b["score"]-a["score"]});
     
 
-    scoreListEl.empty();
+    
     for(var i=0;i<highScores.length;i++){
         const scoreItem = $("<li>");
         scoreItem.text(`${highScores[i]["initials"]}-${highScores[i]["score"]}`);
